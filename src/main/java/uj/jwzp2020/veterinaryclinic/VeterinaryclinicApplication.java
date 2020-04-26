f9447bf68a77267e37a54a9abadf6da38a849428
seqre <seqre@disroot.org>
2020-04-26 23:54:07 +0200

    Initial commit
    Signed-off-by: seqre <seqre@disroot.org>

---
+++ src/main/java/uj/jwzp2020/veterinaryclinic/VeterinaryclinicApplication.java
@@ -0,0 +1,13 @@
+package uj.jwzp2020.veterinaryclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinaryclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinaryclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uj/jwzp2020/veterinaryclinic/VeterinaryclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package uj.jwzp2020.veterinaryclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
