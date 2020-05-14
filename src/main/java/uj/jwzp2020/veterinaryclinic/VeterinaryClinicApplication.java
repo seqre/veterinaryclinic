9d0192584da0e6b682357a07b1cf6010a848ec13
seqre <seqre@disroot.org>
2020-05-14 12:45:14 +0200

    Uppercase app name
    Signed-off-by: seqre <seqre@disroot.org>

--- src/main/java/uj/jwzp2020/veterinaryclinic/VeterinaryClinicApplication.java
+++ src/main/java/uj/jwzp2020/veterinaryclinic/VeterinaryClinicApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VeterinaryclinicApplication {
+public class VeterinaryClinicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VeterinaryclinicApplication.class, args);
+		SpringApplication.run(VeterinaryClinicApplication.class, args);
 	}
 
 }
