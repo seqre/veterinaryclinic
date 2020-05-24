b377ae65b80a19c6683aabcd5c67f3033efb5a4e
seqre <seqre@disroot.org>
2020-05-24 23:34:33 +0200

    Add appointment pet id validation
    Signed-off-by: seqre <seqre@disroot.org>

--- src/main/java/uj/jwzp2020/veterinaryclinic/service/AppointmentService.java
+++ src/main/java/uj/jwzp2020/veterinaryclinic/service/AppointmentService.java
@@ -7,6 +7,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 import uj.jwzp2020.veterinaryclinic.model.appointment.Appointment;
 import uj.jwzp2020.veterinaryclinic.repository.AppointmentRepository;
+import uj.jwzp2020.veterinaryclinic.repository.PetRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -16,11 +17,13 @@ public class AppointmentService {
 
     private final RestTemplate restTemplate;
     private final AppointmentRepository appointmentRepository;
+    private final PetRepository petRepository;
 
     @Autowired
-    public AppointmentService(RestTemplate restTemplate, AppointmentRepository appointmentRepository) {
+    public AppointmentService(RestTemplate restTemplate, AppointmentRepository appointmentRepository, PetRepository petRepository) {
         this.restTemplate = restTemplate;
         this.appointmentRepository = appointmentRepository;
+        this.petRepository = petRepository;
     }
 
     public List<Appointment> getAppointments() {
@@ -33,6 +36,9 @@ public class AppointmentService {
     }
 
     public Appointment save(Appointment appointment) {
+        petRepository.findById(appointment.getPetId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Unknown pet with id " + appointment.getPetId()));
+
         LocalDateTime start = appointment.getDate();
         LocalDateTime end = appointment.getDate().plusMinutes(appointment.getDuration().getMinutes());
 
