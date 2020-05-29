6e0ae0a672320a59e8e60866e339ba286326e3e7
seqre <seqre@disroot.org>
2020-05-29 10:54:06 +0200

    Remove leftover classes
    Signed-off-by: seqre <seqre@disroot.org>

--- src/main/java/uj/jwzp2020/veterinaryclinic/repository/model/client/TransportGender.java
+++
@@ -1,8 +0,0 @@
-package uj.jwzp2020.veterinaryclinic.repository.model.client;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public enum TransportGender {
-    @JsonProperty("male") MALE,
-    @JsonProperty("female") FEMALE
-}
