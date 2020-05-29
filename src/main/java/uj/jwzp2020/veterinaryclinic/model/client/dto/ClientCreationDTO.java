d723d485c367b3b045f7f314784a315afa2cf936
seqre <seqre@disroot.org>
2020-05-29 11:04:59 +0200

    Add ContactValidator tests
    Signed-off-by: seqre <seqre@disroot.org>

--- src/main/java/uj/jwzp2020/veterinaryclinic/model/client/dto/ClientCreationDTO.java
+++ src/main/java/uj/jwzp2020/veterinaryclinic/model/client/dto/ClientCreationDTO.java
@@ -3,7 +3,9 @@ package uj.jwzp2020.veterinaryclinic.model.client.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import uj.jwzp2020.veterinaryclinic.model.serializer.deserializer.StringToLocalDateDeserializer;
 import uj.jwzp2020.veterinaryclinic.model.validation.Contact;
@@ -14,6 +16,8 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @Contact
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ClientCreationDTO {
 
--- src/main/java/uj/jwzp2020/veterinaryclinic/model/validation/ContactValidator.java
+++ src/main/java/uj/jwzp2020/veterinaryclinic/model/validation/ContactValidator.java
@@ -5,6 +5,7 @@ import uj.jwzp2020.veterinaryclinic.model.client.dto.ClientCreationDTO;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+
 public class ContactValidator implements ConstraintValidator<Contact, ClientCreationDTO> {
     @Override
     public boolean isValid(ClientCreationDTO value, ConstraintValidatorContext context) {
